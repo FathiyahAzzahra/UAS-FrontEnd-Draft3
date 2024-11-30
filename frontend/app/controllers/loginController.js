1620d4161ac4794edddc817e5e04c163c9b8f513
Fathiyah Azzahra <fathiyahzzhr@gmail.com>
2024-11-30 18:23:11 +0700

    zara

--- frontend/app/controllers/loginController.js
+++ frontend/app/controllers/loginController.js
@@ -1,18 +1,39 @@
-app.controller('LoginController', function ($scope, $http) {
-    $scope.login = function () {
-        const user = {
-            username: $scope.username,
-            password: $scope.password,
-        };
+// app.controller('LoginController', function ($scope, $http) {
+//     $scope.login = function () {
+//         const user = {
+//             username: $scope.username,
+//             password: $scope.password,
+//         };
 
-        $http.post('/api/data/login', user).then(
-            (response) => {
-                alert(response.data.message);
+//         $http.post('/api/data/login', user).then(
+//             (response) => {
+//                 alert(response.data.message);
+//                 localStorage.setItem('token', response.data.token);
+//             },
+//             (error) => {
+//                 alert(error.data.message);
+//             }
+//         );
+//     };
+// });
+
+var app = angular.module('loginApp', []);
+
+app.controller('LoginController', function ($scope, $http, $window) {
+    $scope.user = {};
+
+    $scope.login = function () {
+        $http.post('http://localhost:3000/api/data/login', $scope.user)
+            .then(function (response) {
+                // Simpan token dan username di localStorage
                 localStorage.setItem('token', response.data.token);
-            },
-            (error) => {
-                alert(error.data.message);
-            }
-        );
+                localStorage.setItem('username', response.data.username);
+
+                alert('Login berhasil!');
+                $window.location.href = '../../index.html'; // Redirect ke halaman utama
+            })
+            .catch(function (error) {
+                alert('Error: ' + error.data.message);
+            });
     };
 });
--- frontend/app/controllers/registerController.js
+++ frontend/app/controllers/registerController.js
@@ -1,18 +1,34 @@
-app.controller('RegisterController', function ($scope, $http) {
-    $scope.register = function () {
-        const user = {
-            username: $scope.username,
-            password: $scope.password,
-        };
+// app.controller('RegisterController', function ($scope, $http) {
+//     $scope.register = function () {
+//         const user = {
+//             username: $scope.username,
+//             password: $scope.password,
+//         };
 
-        $http.post('/api/data/register', user).then(
-            (response) => {
-                alert(response.data.message);
-                window.location.href = '#/login';
-            },
-            (error) => {
-                alert(error.data.message);
-            }
-        );
+//         $http.post('/api/data/register', user).then(
+//             (response) => {
+//                 alert(response.data.message);
+//                 window.location.href = '#/login';
+//             },
+//             (error) => {
+//                 alert(error.data.message);
+//             }
+//         );
+//     };
+// });
+
+var app = angular.module('registerApp', []);
+
+app.controller('RegisterController', function ($scope, $http, $window) {
+    $scope.user = {};
+
+    $scope.register = function () {
+        $http.post('http://localhost:3000/api/data/register', $scope.user)
+            .then(function (response) {
+                alert('Registration successful!');
+                $window.location.href = './login.html'; // Redirect ke halaman login
+            }, function (error) {
+                alert('Error: ' + error.data.message);
+            });
     };
 });
